19f9fd87ccfcc25692636a98a4146199dd6ed6a9
Valeriya Kozhevnikova <v.a.kozh@ya.ru>
2022-01-11 15:33:12 +0300

    feat: edit name and likes

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,9 @@
-/*
-
-клик по кнопке edit 
-показать попап
-заполнить поля текущим именем
-
-клик по сохранить
-скрыть попап
-изменить имя на новое
-
-*/
-
+// Находим имя и род деятельности из профиля в DOM
 let profileName = document.querySelector('.profile__name-text');
 let profileJob = document.querySelector('.profile__about-me');
-
+// Находим попап в DOM
 let popup = document.querySelector('.popup');
+// Находим кнопки в DOM
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = popup.querySelector('.popup__icon-close');
 // Находим форму в DOM
@@ -22,34 +12,44 @@ let formElement = popup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
 
+//вешаем обработчики на клики
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
-function openPopup () {
+//открыть попап
+function openPopup() {
     popup.classList.add('popup_opened');
-    nameInput.value = profileName;
-    jobInput.value = profileJob;
+    //подставляем значения полей
+    nameInput.value = profileName.innerHTML;
+    jobInput.value = profileJob.innerHTML;
 }
 
-function closePopup(){
+//закрыть попап
+function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+// Обработчик «отправки» формы
+function formSubmitHandler(evt) {
+    evt.preventDefault();
 
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
-
-    // Получите значение полей jobInput и nameInput из свойства value
-   // let nameInputValue = nameInput.value;
-    let jobInput = jobInput.value;
+    let nameInputValue = nameInput.value;
+    let jobInputValue = jobInput.value;
     // Выберите элементы, куда должны быть вставлены значения полей
 
     // Вставьте новые значения с помощью textContent
+    profileName.textContent = nameInputValue;
+    profileJob.textContent = jobInputValue;
+    closePopup();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+// Прикрепляем обработчик к форме
 formElement.addEventListener('submit', formSubmitHandler);
+
+//клик по сердечку
+let likes = document.querySelectorAll('.element__like');
+likes.forEach((like) => {
+    like.addEventListener('click', ()=>{
+        like.classList.toggle('element__like_active');
+    })
+})
