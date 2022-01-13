c61daeeb0e259fdc78236a3e519204ef61274aa0
Valeriya Kozhevnikova <v.a.kozh@ya.ru>
2022-01-13 12:55:18 +0300

    fix review

--- scripts/index.js
+++ scripts/index.js
@@ -12,10 +12,6 @@ let formElement = popup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input_type_name');
 let jobInput = formElement.querySelector('.popup__input_type_job');
 
-//вешаем обработчики на клики
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
 //открыть попап
 function openPopup() {
     popup.classList.add('popup_opened');
@@ -29,26 +25,27 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
+
+
 // Обработчик «отправки» формы
 function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    let nameInputValue = nameInput.value;
-    let jobInputValue = jobInput.value;
-
     // Вставьте новые значения с помощью textContent
-    profileName.textContent = nameInputValue;
-    profileJob.textContent = jobInputValue;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     closePopup();
 }
 
 // Прикрепляем обработчик к форме
 formElement.addEventListener('submit', formSubmitHandler);
-
+// вешаем обработчики на клики
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 // Клик по сердечку
-let likes = document.querySelectorAll('.element__like');
+/*let likes = document.querySelectorAll('.element__like');
 likes.forEach((like) => {
     like.addEventListener('click', ()=>{
         like.classList.toggle('element__like_active');
     })
-})
+})*/
