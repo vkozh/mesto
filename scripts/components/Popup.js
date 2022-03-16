30e29388304e910396507eef14f154c159c7f43e
Lekozhe <v.a.kozh@ya.ru>
2022-03-16 18:07:43 +0300

    add popup class

---
+++ scripts/components/Popup.js
@@ -0,0 +1,35 @@
+import {
+    popupButtonCLoseSelector
+} from '../utils/constants.js'
+
+export default class Popup {
+    constructor(selector) {
+        this._popup = document.querySelector(selector);
+    }
+
+    open() {
+        this._popup.classList.add('popup_opened');
+    }
+
+    close() {
+        this._popup.classList.remove('popup_opened');
+    }
+
+    _closeOnOverlay(e) {
+        if (e.target === e.currentTarget) {
+            this.close();
+        }
+    }
+
+    _handleEscClose(e) {
+        if (e.key === 'Escape') {
+            this.close();
+        }
+    }
+
+    setEventListeners() {
+        document.addEventListener('keydown', (e) => this._handleEscClose(e));
+        this._popup.querySelector(popupButtonCLoseSelector).addEventListener('click', () => this.close());
+        this._popup.addEventListener('click', (e) => this._closeOnOverlay(e))
+    }
+}
