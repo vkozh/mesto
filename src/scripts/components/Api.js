dc3cd4f5a5b81a291cfdc363fff01694bd7eaa48
Lekozhe <v.a.kozh@ya.ru>
2022-04-01 15:32:35 +0300

    refactor fetch function, create checkResponce

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -1,74 +1,77 @@
 export default class Api {
-    constructor({baseUrl, headers, renderLoading}) {
+    constructor({
+        baseUrl,
+        headers,
+        renderLoading
+    }) {
         this._url = baseUrl;
         this._headers = headers;
         this._renderLoading = renderLoading;
     }
 
-    _fetchMain(path, method) {
-        return fetch(`${this._url}${path}`, {
-                method: method,
-                headers: this._headers
-            })
-            .then(res => {
-                if (res.ok)
-                    return res.json();
-                return Promise.reject(`Ошибка ${res.status}`);
-            });
+    _fetch(path, method, bodyObject, button) {
+        if (bodyObject === undefined)
+            return fetch(`${this._url}${path}`, {
+                    method: method,
+                    headers: this._headers
+                })
+                .then(this._checkResponse);
+        else {
+            const text = button.textContent;
+            this._renderLoading(true, button);
+            return fetch(`${this._url}${path}`, {
+                    method: method,
+                    headers: this._headers,
+                    body: JSON.stringify(bodyObject)
+                })
+                .then(this._checkResponse)
+                .finally(() => this._renderLoading(false, button, text));
+        }
     }
 
-    _fetchProfile(path, method, bodyObject, button) {
-        const text = button.textContent;
-        this._renderLoading(true, button);
-        return fetch(`${this._url}${path}`, {
-                method: method,
-                headers: this._headers,
-                body: JSON.stringify(bodyObject)
-            })
-            .then(res => {
-                if (res.ok)
-                    return res.json();
-                return Promise.reject(`Ошибка ${res.status}`);
-            })
-            .finally(() => this._renderLoading(false, button, text));
+    _checkResponse(res) {
+        if (res.ok)
+            return res.json();
+        return Promise.reject(`Ошибка ${res.status}`);
     }
+
 
     getUser() {
-        return this._fetchMain('/users/me', 'GET');
+        return this._fetch('/users/me', 'GET');
     }
 
     getCards() {
-        return this._fetchMain('/cards', 'GET');
+        return this._fetch('/cards', 'GET');
     }
 
     editProfile(name, about, button) {
-        return this._fetchProfile('/users/me', 'PATCH', {
+        return this._fetch('/users/me', 'PATCH', {
             name: name,
             about: about
         }, button);
     }
 
     addCard(name, link, button) {
-        return this._fetchProfile('/cards', 'POST', {
+        return this._fetch('/cards', 'POST', {
             name: name,
             link: link
         }, button);
     }
 
     deleteCard(cardId) {
-        return this._fetchMain(`/cards/${cardId}`, 'DELETE');
+        return this._fetch(`/cards/${cardId}`, 'DELETE');
     }
 
     setLike(cardId) {
-        return this._fetchMain(`/cards/${cardId}/likes`, 'PUT');
+        return this._fetch(`/cards/${cardId}/likes`, 'PUT');
     }
 
     deleteLike(cardId) {
-        return this._fetchMain(`/cards/${cardId}/likes`, 'DELETE');
+        return this._fetch(`/cards/${cardId}/likes`, 'DELETE');
     }
 
     changeAvatar(avatar, button) {
-        return this._fetchProfile('/users/me/avatar', 'PATCH', {
+        return this._fetch('/users/me/avatar', 'PATCH', {
             avatar: avatar
         }, button);
     }
