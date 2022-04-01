6b77cb09aa130f08ee7082a55f14468320d6d9f9
Lekozhe <v.a.kozh@ya.ru>
2022-04-01 17:06:47 +0300

    add id

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -13,13 +13,15 @@ export default class UserInfo {
         return {
             name: this._userNameElement.textContent,
             job: this._userAboutElement.textContent,
-            avatar: this._userAvatarSelector.src
+            avatar: this._userAvatarSelector.src,
+            id: this._id
         }
     }
 
-    setUserInfo({name, about, avatar}) {
+    setUserInfo({name, about, avatar, _id}) {
         this._userNameElement.textContent = name;
         this._userAboutElement.textContent = about;
         this._userAvatarSelector.src = avatar;
+        this._id = _id;
     }
 }
